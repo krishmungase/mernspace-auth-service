16c2255fdd18d8d480e01d97304f377909991482
krishmungase <mungasekrishna8@gmail.com>
2025-01-12 14:35:38 +0530

    add test setup

---
+++ app.spec.ts
@@ -0,0 +1,16 @@
+import app from "./src/app";
+import request from "supertest"
+import { calculateDiscount } from "./src/utils"
+
+describe("App", () => {
+
+  it("should return correct discount amount" ,() => {
+    const discount = calculateDiscount(100,10)
+    expect(discount).toBe(10);
+  })
+
+  it("should return 200 status code",async ()=>{
+    const response = await request(app).get('/').send();
+    expect(response.statusCode).toBe(200)
+  })
+})
