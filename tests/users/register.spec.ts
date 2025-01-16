8006924f141f2ed6f659cbd297f7212479cd52ed
krishmungase <mungasekrishna8@gmail.com>
2025-01-16 16:04:15 +0530

    write first test

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -18,7 +18,46 @@ describe("POST /auth/register", () => {
       // Assert
 
       expect(response.statusCode).toBe(201)
+    });
+
+    it("should return valid json response", async () => {
+      // AAA
+
+      // Arrange
+      const  userData = {
+        firstName: "Krish",
+        lastName: "M",
+        email: "krishmungase@gmail.com",
+        password:"secret"
+      }
+      // Act
+      const response = await request(app).post('/auth/register').send(userData)
+      // Assert
+
+      expect((response.headers as Record<string,string>)['content-type']).toEqual(expect.stringContaining("json"))
+    });
+
+    it("should persist the user in the database", async () => {
+       // AAA
+
+      // Arrange
+      const  userData = {
+        firstName: "Krish",
+        lastName: "M",
+        email: "krishmungase@gmail.com",
+        password:"secret"
+      }
+
+      // Act
+      await request(app).post('/auth/register').send(userData)
+
+      // Assert
+
+
     })
+
+
+    
   })
   describe("Fields are missing ",() => {})
 })
