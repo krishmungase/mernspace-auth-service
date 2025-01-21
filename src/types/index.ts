611dc570fd01d1bd8d19343bffe87efdfe2c3dfb
krishmungase <mungasekrishna8@gmail.com>
2025-01-21 20:43:05 +0530

    add types of AuthRequest

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,3 +10,11 @@ export interface UserData {
 export interface RegisterUserRequest extends Request {
   body: UserData;
 }
+
+
+export interface AuthRequest extends Request{
+  auth: {
+    id: number;
+    role: number;
+  }
+}
