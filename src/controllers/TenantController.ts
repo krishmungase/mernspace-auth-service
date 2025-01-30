eb2ae062340c4cb65fb6a9be573973fbdd2573be
krishmungase <mungasekrishna8@gmail.com>
2025-01-30 19:19:27 +0530

    feat: add create tenant endpoint

---
+++ src/controllers/TenantController.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import { TenantService } from "../services/TenantService";
+import { CreateTenantRequest } from "../types";
+import { Logger } from "winston";
+
+export class TenantController {
+  constructor(private tenantService: TenantService, private logger: Logger) {}
+  async create(req: CreateTenantRequest, res: Response, next: NextFunction) {
+    const { name, address } = req.body;
+
+    try {
+      const tenant = await this.tenantService.create({ name, address });
+      this.logger.info("Tenant created", { id: tenant.id });
+      res.status(201).json({ id: tenant.id });
+    } catch (err) {
+      next(err);
+      return;
+    }
+  }
+}
