efddcee9ee0585f0ff13040135cf0e06d86e164e
krishmungase <mungasekrishna8@gmail.com>
2025-01-30 18:18:22 +0530

    feat: add tenant tables

--- src/entity/Tenant.ts
+++ src/entity/Tenant.ts
@@ -2,22 +2,22 @@ import {
   Entity,
   PrimaryGeneratedColumn,
   Column,
-  ManyToOne,
   UpdateDateColumn,
   CreateDateColumn,
 } from "typeorm";
-import { User } from "./User";
 
-@Entity({name: "refreshTokens"})
-export class RefreshToken {
+
+
+@Entity({name: "tenants"})
+export class Tenant {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: "timestamp" })
-  expiresAt: Date;
+  @Column('varchar',{length: 100})
+  name: string;
 
-  @ManyToOne(() => User)
-  user: User;
+  @Column('varchar',{length: 255})
+  address: string;
 
   @UpdateDateColumn()
   updatedAt: number;
